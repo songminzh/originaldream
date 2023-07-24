7f697ae3f136b3e2eb896c3772d74952eec234ad
zhengmin <zhengmin@laiyouxi.com>
2023-07-24 14:27:04 +0800

    Exercises

--- Swift/007_enum.playground/Contents.swift
+++ Swift/007_enum.playground/Contents.swift
@@ -90,4 +90,57 @@ let expression = ArithmeticExpression.addition(
 
 print(evaluate(expression))
 
+// 练习
+enum ShapeDimensions {
+    // 点 没有关联值，它没有尺寸
+    case point
+    // 正方形的关联值是边长
+    case square(side: Double)
+    // 长方形的关联值是宽和高
+    case rectangle(width: Double, height: Double)
+    // 直角三角形
+    case rightTriangle(cathetus1: Double, cathetus2: Double)
+    
+    // 利用关联值计算面积
+    func area() -> Double {
+        switch self {
+        case .point:
+            return 0.0
+        case let .square(side: side):
+            return side * side
+        case let .rectangle(width: w, height: h):
+            return w * h
+        case let .rightTriangle(cathetus1: c1, cathetus2: c2):
+            return (c1 * c2) / 2
+        }
+    }
+    
+    // 利用关联值计算周长
+    func perimeter() -> Double {
+        switch self {
+        case .point:
+            return 0.0
+        case let .square(side: side):
+            return 4 * side
+        case let .rectangle(width: width, height: height):
+            return 2 * (width + height)
+        case let .rightTriangle(cathetus1: c1, cathetus2: c2):
+            let hypotenuse = sqrt(c1*c1 + c2*c2)
+            return (c1 + c2) + hypotenuse
+        }
+    }
+    
+}
 
+var squareShape = ShapeDimensions.square(side: 10.0)
+var rectShape = ShapeDimensions.rectangle(width: 5.0, height: 10.0)
+var rightTriangle = ShapeDimensions.rightTriangle(cathetus1: 3, cathetus2: 4)
+var point = ShapeDimensions.point
+print("Square shape's area is \(squareShape.area())")
+print("Rect shape's area is \(rectShape.area())")
+print("Right triangle's area is \(rightTriangle.area())")
+print("Point's area is \(point.area())")
+print("Rect shape's preimeter is \(rectShape.perimeter())")
+print("Square shape's preimeter is \(squareShape.perimeter())")
+print("Right triangle's preimeter is \(rightTriangle.perimeter())")
+print("Point's preimeter is \(point.perimeter())")
